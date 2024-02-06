0c58d58f7b23556c9c6c3403586a876ae03e865a
Karim Jabbour <kareemjabbour@gmail.com>
2024-02-06 01:28:24 -0500

    Minor fix

--- src/app.c
+++ src/app.c
@@ -10,25 +10,32 @@ void hello(char *s, char *r) {
 
 int main() {
     int rv;
+        char *names[10];
+
     rv = zcs_init(ZCS_APP_TYPE);
-        for (int i = 0; i < 1000; i++) {
+    sleep(10);
+        rv = zcs_query("type", "speaker", names, 10);
+
+        for (int i = 0; i < 5; i++) {
         // rv = zcs_post_ad("mute", "on");
-        sleep(10);
+        // sleep(10);
         // rv = zcs_post_ad("mute", "off");
-        sleep(10);
+        sleep(2);
+
     }
-    char *names[10];
-    rv = zcs_query("type", "speaker", names, 10);
+
     if (rv > 0) {
-        printf("QUERIED\n");
-        zcs_attribute_t attrs[5];}
+        printf("QUERIED\n");}
+    // sleep(4);
+
+    //     zcs_attribute_t attrs[5];
 	// int anum = 5;
     //     rv = zcs_get_attribs(names[0], attrs, &anum);
     //     if ((strcmp(attrs[0].attr_name, "location") == 0) &&
     //         (strcmp(attrs[0].value, "kitchen") == 0)) {
-    //             rv = zcs_listen_ad(names[0], hello);
+    //             // rv = zcs_listen_ad(names[0], hello);
     //     }
-    // }
-}
+    }
+
 
 
--- src/zcs.c
+++ src/zcs.c
@@ -96,8 +96,6 @@ void decode_notification(char *message, LocalTableEntry *entry) {
       char *attr_name = token;
       char *value = equalSign + 1;
       if (strcmp(attr_name, "message_type") == 0) {
-        // ignore the type field
-      if (strcmp(attr_name, "message_type") == 0) {
         // ignore the message_type field
       } else if (strcmp(attr_name, "name") == 0) {
         entry->serviceName = strdup(value);
